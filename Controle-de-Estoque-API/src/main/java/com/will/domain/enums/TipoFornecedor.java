c23c42508bf02be0ef060ac7eb8d5b2e75a9a1aa
thaleswillreis <thaleswill@yahoo.com.br>
2023-08-09 11:24:27 -0300

    Database instantiation and implementation of domain classes

---
+++ Controle-de-Estoque-API/src/main/java/com/will/domain/enums/TipoFornecedor.java
@@ -0,0 +1,36 @@
+package com.will.domain.enums;
+
+public enum TipoFornecedor {
+
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+
+	private int cod;
+	private String descricao;
+
+	private TipoFornecedor(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public static TipoFornecedor toEnum(Integer cod) {
+
+		if (cod == null) {
+			return null;
+		}
+		for (TipoFornecedor x : TipoFornecedor.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
